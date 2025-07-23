c6613ea03f3b22e362dfe85d8449a0e1fca3092f
Ava <avasophianorgaard@gmail.com>
2025-07-23 11:02:08 -0700

    Initial Commit

---
+++ gitPracticeTests/gitPracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  gitPracticeTests.swift
+//  gitPracticeTests
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import Testing
+@testable import gitPractice
+
+struct gitPracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
